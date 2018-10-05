c7e02539ce54622e0c859b4781d451df861d34ea
Nicaise AYIMANDE <sedjronicaise@gmail.com>
2018-10-05 08:01:15 +0200

    config app name

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="text-center">
 
-    &copy; {{date('Y')}} larcarte realisé par Nicaise <a href="https://nico-tech.net">Power Communication</a> 
-    <p><b>Laracarte est mon premier application laravel</b></p>
+    &copy; {{date('Y')}} {{config('app.name')}} realisé par Nicaise <a href="https://nico-tech.net">Power Communication</a> 
+    <p><b>{{config('app.name')}} est mon premier application laravel</b></p>
     <p>C'est un simple et petite application permettant de connaitre ma position géographique </p>
 </footer>
